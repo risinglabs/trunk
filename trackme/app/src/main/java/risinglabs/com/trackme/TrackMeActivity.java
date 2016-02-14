6b0f46b62ad379001f724032cf9167397422ff8d
risinglabs <risinglabs@gmail.com>
2016-02-14 03:18:24 -0800

    Adding Share and multi selection dialog for tracking

--- trackme/app/src/main/java/risinglabs/com/trackme/TrackMeActivity.java
+++ trackme/app/src/main/java/risinglabs/com/trackme/TrackMeActivity.java
@@ -1,27 +1,57 @@
 package risinglabs.com.trackme;
 
 import android.content.Context;
+import android.content.Intent;
+import android.graphics.Color;
 import android.location.Criteria;
 import android.location.Location;
 import android.location.LocationManager;
 import android.os.Bundle;
-import android.support.v4.app.FragmentActivity;
+import android.support.v4.view.MenuItemCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.ShareActionProvider;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.Circle;
+import com.google.android.gms.maps.model.CircleOptions;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class TrackMeActivity extends FragmentActivity implements OnMapReadyCallback {
+public class TrackMeActivity extends AppCompatActivity implements OnMapReadyCallback,
+        View.OnClickListener,
+        GoogleMap.OnMarkerClickListener {
 
     private GoogleMap mMap;
+    private Button trackButton;
+    private Location myLocation;
+    private MarkerOptions markerOptions;
+    private ShareActionProvider mShareActionProvider;
+    private TrackOptionsDialog optionsDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_track_me);
+
+        trackButton = (Button)findViewById(R.id.locatebutton);
+        trackButton.setOnClickListener(this);
+
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
+
+
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
@@ -47,16 +77,106 @@ public class TrackMeActivity extends FragmentActivity implements OnMapReadyCallb
                 getSystemService(Context.LOCATION_SERVICE);
         Criteria criteria = new Criteria();
 
-        Location myLocation = locationManager.getLastKnownLocation(locationManager
+        myLocation = locationManager.getLastKnownLocation(locationManager
                 .getBestProvider(criteria, false));
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.settings, menu);
+
+        // Locate MenuItem with ShareActionProvider
+        MenuItem item = menu.findItem(R.id.menu_item_share);
+
+        mShareActionProvider = (ShareActionProvider) MenuItemCompat.getActionProvider(item);
+        //mShareActionProvider = (ShareActionProvider) item.getActionProvider();
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.settings:
+                Toast.makeText(this, "This is Settings", Toast.LENGTH_LONG).show();
+                return true;
+
+            case R.id.help:
+                Toast.makeText(this, "This is Help", Toast.LENGTH_LONG).show();
+                return true;
+
+            case R.id.menu_item_share:
+                Intent sendIntent = new Intent();
+                sendIntent.setAction(Intent.ACTION_SEND);
+                sendIntent.setType("text/plain");
+                sendIntent.putExtra(Intent.EXTRA_TEXT, "This is my text to send.");
+                setShareIntent(sendIntent);
+                return true;
+
+            default:
+                // If we got here, the user's action was not recognized.
+                // Invoke the superclass to handle it.
+                return super.onOptionsItemSelected(item);
+
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        trackButton.setVisibility(View.GONE);
 
         // Add a marker in Sydney and move the camera
         LatLng mylatLong = new LatLng(myLocation.getLatitude(), myLocation.getLongitude());
-        mMap.addMarker(new MarkerOptions().position(mylatLong).title("My Location"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(mylatLong));
+
+        mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
 
 
 
+        // You can customize the marker image using images bundled with
+        // your app, or dynamically generated bitmaps.
+//        map.addMarker(new MarkerOptions()
+//                .icon(BitmapDescriptorFactory.fromResource(R.drawable.house_flag))
+//                .anchor(0.0f, 1.0f) // Anchors the marker on the bottom left
+//                .position(new LatLng(41.889, -87.622)));
+
+        // Add a circle in Sydney
+        Circle circle = mMap.addCircle(new CircleOptions()
+                .center(mylatLong)
+                .radius(10)
+                .strokeColor(Color.RED)
+                .fillColor(Color.TRANSPARENT));
+
+        markerOptions = new MarkerOptions();
+
+
+        mMap.addMarker(
+                markerOptions
+                        .position(mylatLong)
+                        .title("My Location")
+                        .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE)));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(mylatLong, 16));
+
+        mMap.setMyLocationEnabled(false);
+        mMap.setOnMarkerClickListener(this);
+
+    }
+
+    // Call to update the share intent
+    private void setShareIntent(Intent shareIntent) {
+        if (mShareActionProvider != null) {
+            Toast.makeText(this, "This is share", Toast.LENGTH_LONG).show();
+            mShareActionProvider.setShareIntent(shareIntent);
+        }
+    }
+
+    @Override
+    public boolean onMarkerClick(Marker marker) {
+        //Toast.makeText(this, "This is my Location", Toast.LENGTH_LONG).show();
+        optionsDialog = new TrackOptionsDialog();
+        optionsDialog.show(getSupportFragmentManager(), "Options");
+        return true;
     }
 }
