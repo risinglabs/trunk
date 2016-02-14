f90e08b5ee390e7656615a4621ba0d6f79da6514
risinglabs <risinglabs@gmail.com>
2016-02-14 03:19:19 -0800

    Adding Share and multi selection dialog for tracking

---
+++ trackme/app/src/main/java/risinglabs/com/trackme/TrackOptionsDialog.java
@@ -0,0 +1,58 @@
+package risinglabs.com.trackme;
+
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
+import android.support.v7.app.AlertDialog;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+
+public class TrackOptionsDialog extends DialogFragment {
+
+    private ArrayList mSelectedItems;
+
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        mSelectedItems = new ArrayList();  // Where we track the selected items
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        // Set the dialog title
+        builder.setTitle(R.string.myoptions)
+                // Specify the list array, the items to be selected by default (null for none),
+                // and the listener through which to receive callbacks when items are selected
+                .setMultiChoiceItems(R.array.multipleoptions, null,
+                        new DialogInterface.OnMultiChoiceClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which,
+                                                boolean isChecked) {
+                                if (isChecked) {
+                                    // If the user checked the item, add it to the selected items
+                                    mSelectedItems.add(which);
+                                } else if (mSelectedItems.contains(which)) {
+                                    // Else, if the item is already in the array, remove it
+                                    mSelectedItems.remove(Integer.valueOf(which));
+                                }
+                            }
+                        })
+                        // Set the action buttons
+                .setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int id) {
+                        // User clicked OK, so save the mSelectedItems results somewhere
+                        // or return them to the component that opened the dialog
+                        Toast.makeText(getActivity().getApplicationContext(), "On Click", Toast.LENGTH_LONG).show();
+                    }
+                })
+                .setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int id) {
+                        Toast.makeText(getActivity().getApplicationContext(), "On Cancel", Toast.LENGTH_LONG).show();
+                    }
+                });
+
+        return builder.create();
+    }
+}
